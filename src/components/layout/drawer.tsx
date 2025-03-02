2bf64cefff99f8fa51d06a02b150a92eac678ab3
noway777 <ahmadrezamohammadi777@gmail.com>
2025-03-02 20:44:25 +0330

    fix: drawer bug fixed

--- src/components/layout/drawer.tsx
+++ src/components/layout/drawer.tsx
@@ -14,12 +14,10 @@ import {
   SALE_ROUTE,
 } from "@/router/const";
 
-import {  RiMenu3Fill } from "react-icons/ri";
+import { RiMenu3Fill } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
 import { AiFillProduct } from "react-icons/ai";
 import Logo from "../../logo/logo";
-
-
 
 export const DrawerPanel = () => {
   const navigate = useNavigate();
@@ -40,74 +38,71 @@ export const DrawerPanel = () => {
         <DrawerContext>
           {(store) => (
             <DrawerBody>
+              <Box
+                flexDirection={"column"}
+                display={"flex"}
+                gap={8}
+                marginTop={20}
+              >
+                <Logo />
+                <Button
+                  onClick={() => navigate(PROFILE_ROUTE)}
+                  colorScheme="green"
+                  variant="ghost"
+                  _hover={{
+                    border: "none",
+                    borderColor: "green.500",
+                    bg: "green.100",
+                    boxShadow: "lg",
+                  }}
+                  display="flex"
+                  gap={2}
+                >
+                  Profile <CgProfile />
+                </Button>
 
-         <Box flexDirection={"column"} display={"flex"} gap={8} marginTop={20}>
-              <Logo/>
-  <Button 
-    onClick={() => navigate(PROFILE_ROUTE)} 
-    colorScheme="green"
-    variant="ghost"
-    _hover={{
-      border: "none", 
-      borderColor: "green.500", 
-      bg: "green.100", 
-      boxShadow: "lg" 
-     _hover={{ background: "gray.600", color: "white" }}
-    }} 
-    display="flex" 
-    gap={2}
-  >
-    Profile <CgProfile />
-  </Button>
+                <Button
+                  onClick={() => navigate(PRODUCT_ROUTE)}
+                  colorScheme="green"
+                  variant="ghost"
+                  _hover={{
+                    border: "none",
+                    borderColor: "green.500",
+                    bg: "green.100",
+                    boxShadow: "lg",
+                  }}
+                >
+                  Product <AiFillProduct />
+                </Button>
 
-  <Button 
-    onClick={() => navigate(PRODUCT_ROUTE)} 
-    colorScheme="green"
-    variant="ghost"
-    _hover={{
-      border: "none", 
-      borderColor: "green.500", 
-      bg: "green.100",
-      boxShadow: "lg" 
-    _hover={{ background: "gray.600", color: "white" }}
-    }}
-  >
-    Product <AiFillProduct />
-  </Button>
+                <Button
+                  onClick={() => navigate(SALE_ROUTE)}
+                  colorScheme="green"
+                  variant="ghost"
+                  _hover={{
+                    border: "none",
+                    borderColor: "green.500",
+                    bg: "green.100",
+                    boxShadow: "lg",
+                  }}
+                >
+                  Sales
+                </Button>
 
-  <Button 
-    onClick={() => navigate(SALE_ROUTE)} 
-    colorScheme="green"
-    variant="ghost"
-    _hover={{
-      border: "none", 
-      borderColor: "green.500", 
-      bg: "green.100",
-      boxShadow: "lg"
-      _hover={{ background: "gray.600", color: "white" }}
-    }}
-  >
-    Sales
-  </Button>
-
-  <Button 
-    onClick={() => navigate(ADD_PRODUCTS_ROUTE)} 
-    colorScheme="green"
-    variant="ghost"
-    _hover={{
-      border: "none", 
-      borderColor: "green.500", 
-      bg: "green.100",
-      boxShadow: "lg"
-                     _hover={{ background: "gray.600", color: "white" }}
-    }}
-  >
-    Add Product
-  </Button>
-</Box>
-
-
-
+                <Button
+                  onClick={() => navigate(ADD_PRODUCTS_ROUTE)}
+                  colorScheme="green"
+                  variant="ghost"
+                  _hover={{
+                    border: "none",
+                    borderColor: "green.500",
+                    bg: "green.100",
+                    boxShadow: "lg",
+                  }}
+                >
+                  Add Product
+                </Button>
+              </Box>
             </DrawerBody>
           )}
         </DrawerContext>
