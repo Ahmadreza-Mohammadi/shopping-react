5124e8e63be3171329cb237ffe4f1062f7670615
noway777 <ahmadrezamohammadi777@gmail.com>
2025-03-06 01:53:55 +0330

    feat: dashboard animation added

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,13 +5,12 @@ export default function Home () {
     <div>
       <Header />
       <div className="w-full h-screen bg-white text-black relative flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-10 rounded-2xl shadow-2xl w-[750px] h-[500px] bg-white p-6">
-          <img src="src/assets/pngs/fruits.png" alt="logo" className="w-110"/>
-          <p className="home-header">به پنل ادمین ارگانیک شاپ خوش آمدید.</p>
+        <div className="flex flex-col justify-center items-center gap-10 rounded-2xl shadow-2xl w-[750px] h-[500px] bg-white p-6 animation-fade-in">
+          <img src="src/assets/pngs/fruits.png" alt="logo" className="w-110 animation-slide-in"/>
+          <p className="home-header text-center">به پنل ادمین ارگانیک شاپ خوش آمدید.</p>
         </div>
       </div>
+    
     </div>
   );
 };
-
-
