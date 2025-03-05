f52fea5db0d39806a900a420024bd9b4a617ba26
noway777 <ahmadrezamohammadi777@gmail.com>
2025-03-05 16:26:49 +0330

    fix: table colors fixed

--- src/components/useres/users.tsx
+++ src/components/useres/users.tsx
@@ -31,28 +31,28 @@ export default function Users() {
             <Stack gap="10">
               <div className="max-h-80  border border-gray-300 rounded-md shadow-lg">
                 <Table.Root>
-                  <Table.Header className="sticky top-0 bg-white z-10 shadow-md">
-                    <Table.Row>
-                      <Table.ColumnHeader>نام کاربر</Table.ColumnHeader>
-                      <Table.ColumnHeader>ایمیل</Table.ColumnHeader>
-                      <Table.ColumnHeader>تاریخ عضویت</Table.ColumnHeader>
-                      <Table.ColumnHeader>نقش کاربر</Table.ColumnHeader>
-                      <Table.ColumnHeader>رمز کاربر</Table.ColumnHeader>
-                      <Table.ColumnHeader>وضعیت حساب</Table.ColumnHeader>
-                      <Table.ColumnHeader>تعداد ورود ها</Table.ColumnHeader>
-                      <Table.ColumnHeader>مشاهده کاربر </Table.ColumnHeader>
+                  <Table.Header className="sticky top-0 bg-white z-10 shadow-2xl">
+                    <Table.Row background={"white"}>
+                      <Table.ColumnHeader color={"black"}>نام کاربر</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>ایمیل</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>تاریخ عضویت</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>نقش کاربر</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>رمز کاربر</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>وضعیت حساب</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>تعداد ورود ها</Table.ColumnHeader>
+                      <Table.ColumnHeader color={"black"}>مشاهده کاربر </Table.ColumnHeader>
                     </Table.Row>
                   </Table.Header>
                   <Table.Body>
                     {items.map((item) => (
-                      <Table.Row key={item.id}>
-                        <Table.Cell>{item.name}</Table.Cell>
-                        <Table.Cell>{item.email}</Table.Cell>
-                        <Table.Cell>{item.date}</Table.Cell>
-                        <Table.Cell>{item.role}</Table.Cell>
-                        <Table.Cell>{item.password}</Table.Cell>
-                        <Table.Cell>{item.status}</Table.Cell>
-                        <Table.Cell>{item.logIn}</Table.Cell>
+                      <Table.Row key={item.id} background={"white"}>
+                        <Table.Cell color={"black"}>{item.name}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.email}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.date}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.role}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.password}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.status}</Table.Cell>
+                        <Table.Cell color={"black"}>{item.logIn}</Table.Cell>
                         <Table.Cell onClick={() => navigate(USERPROFILE_ROUTE)}>
                           *
                         </Table.Cell>
